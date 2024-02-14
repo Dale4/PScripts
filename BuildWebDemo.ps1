bd70695583db2c35212a3a251a21f4d0de5a2672
Dale O'Neill <add.daleoneill@gmail.com>
2024-02-13 23:19:49 -0800

    Updated script with console messages

--- BuildWebDemo.ps1
+++ BuildWebDemo.ps1
@@ -12,23 +12,30 @@ $originalPath = $PWD.Path
 
 Write-Host "Start build ..."
 
+Write-Host "Create solution " $solutionName
 dotnet new sln --output $solutionName
 cd $solutionName
 
+Write-Host "Build Web Api project " $webapiProjName
 #Build Web API project
 dotnet new webapi --output $webapiProjName
 dotnet sln add $webapiProjName
 cd $webapiProjName
+Write-Host "Publish image to local docker registry " $webapiProjName":"$imagetag
 dotnet add package Microsoft.NET.Build.Containers
 dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer
 dotnet publish --os linux --arch x64 -p:PublishProfile=DefaultContainer -p:ContainerImageTag=$imagetag
 cd ..
 
+Write-Host "Build Web project " $webProjName
 #Build Web Project
 dotnet new web --output $webProjName
 dotnet sln add $webProjName
 cd $webProjName
+Write-Host "Publish image to local docker registry " $webProjName":"$imagetag 
 dotnet add package Microsoft.NET.Build.Containers
 dotnet publish --os linux --arch x64 -p:PublishProfile=DefaultContainer -p:ContainerImageTag=$imagetag
+
+Write-Host "Build Completed"
 
 Set-Location -Path $originalPath
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PScripts
 Location where I keep my power shell scripts 
+
+run this from the commandline
+./BuildWebDemo.ps1 -solutionName demo1 -imagetag 1.1.8 > $null 2>&1
